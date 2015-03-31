58facabccfaa1f86f28e8a53db10aaf52ab87f76
Vasiliy Loginevskiy <yeti-or@yandex-team.ru>
2015-03-31 03:49:39 +0300

    Add osX config.

---
+++ os_x.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# Enable 3-finger drag. (Moving with 3 fingers in any window "chrome" moves the window.)
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeFingerDrag -bool true
+defaults write com.apple.AppleMultitouchTrackpad TrackpadThreeFingerDrag -bool true
